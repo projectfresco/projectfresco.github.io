5e8d4ee87bb15b6523d8c05a971b44180788d9cd
FranklinDM <mrmineshafter17@gmail.com>
2022-07-13 01:43:40 +0800

    Increment content revision

--- .base/fresco-main/src/assets/main.js
+++ .base/fresco-main/src/assets/main.js
@@ -1,6 +1,6 @@
 const URL_APP_BASE = "/addons/";
 
-const CONTENT_REVISION = "?rel=1656780015";
+const CONTENT_REVISION = "?rel=1657676397";
 
 const URL_CONTENT = "/addons-content/";
 const URL_CONTENT_METADATA = URL_CONTENT + "metadata.json" + CONTENT_REVISION;
